3b54b3c539f4842d9c36bd5fe9c46098e75a55b0
derbenx <colin@derben.ca>
2023-09-12 00:31:17 -0400

    Delete templates directory

--- templates/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-script('solitaire', 'cards');
-script('solitaire', 'main');
-style('solitaire', 'sol');
-?>
-
-<?php print_unescaped($this->inc('content/index')); ?>
-<?php //print_unescaped($this->inc('content/game')); ?>
